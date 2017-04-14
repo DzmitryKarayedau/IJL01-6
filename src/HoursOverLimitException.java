afa94e7e53f578373513347604d74a3a464652f0
siarhei.miashkou <siarhei.miashkou@leverx.com>
2017-04-14 09:15:39 +0300

    Hours exception

---
+++ src/HoursOverLimitException.java
@@ -0,0 +1,11 @@
+/**
+ * Created by dzmitry.karayedau on 13-Apr-17.
+ */
+public class HoursOverLimitException extends Exception {
+    public HoursOverLimitException() {
+    }
+
+    public HoursOverLimitException(String s) {
+        super(s);
+    }
+}
