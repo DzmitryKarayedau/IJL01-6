e5dd00ad1814f616a2555fc5860e796057ecf5a6
siarhei.miashkou <siarhei.miashkou@leverx.com>
2017-04-18 09:39:25 +0300

    Replace double to BigDecimal

--- src/com/emerline/ijl01_6/Main.java
+++ src/com/emerline/ijl01_6/Main.java
@@ -4,6 +4,7 @@ import com.emerline.ijl01_6.exceptions.HoursOverLimitException;
 import com.emerline.ijl01_6.exceptions.PriceOverLimitException;
 import com.emerline.ijl01_6.utils.PayCalculator;
 
+import java.math.BigDecimal;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,15 +12,15 @@ public class Main {
 
     public static void main(String[] args) throws HoursOverLimitException, PriceOverLimitException {
         Scanner in = new Scanner(System.in);
-        float workingHours;
-        float pricePerHour;
+        BigDecimal workingHours;
+        BigDecimal pricePerHour;
 
         try {
             System.out.print("Working hours count: ");
-            workingHours = in.nextFloat();
+            workingHours = in.nextBigDecimal();
             System.out.print("Price per hour: ");
-            pricePerHour = in.nextFloat();
-            System.out.printf("Salary = %f%n ", PayCalculator.calculatePay(pricePerHour, workingHours));
+            pricePerHour = in.nextBigDecimal();
+            System.out.printf("Salary = %.2f%n ", PayCalculator.calculatePay(pricePerHour, workingHours));
         } catch (InputMismatchException e) {
             System.err.println("Wrong input data");
         } catch (HoursOverLimitException e) {
--- src/com/emerline/ijl01_6/utils/PayCalculator.java
+++ src/com/emerline/ijl01_6/utils/PayCalculator.java
@@ -3,37 +3,40 @@ package com.emerline.ijl01_6.utils;
 import com.emerline.ijl01_6.exceptions.HoursOverLimitException;
 import com.emerline.ijl01_6.exceptions.PriceOverLimitException;
 
+import java.math.BigDecimal;
+
 /**
  * Created by dzmitry.karayedau on 13-Apr-17.
  */
 public class PayCalculator {
-    private final static float MIN_PRICE_PER_HOUR = 8;
-    private final static float MAX_HOURS_PER_WEEK = 60;
-    private final static float LIMIT_HOURS_TO_KOEFF = 40;
-    private final static float AFTER_LIMIT_KOEFFICIENT = 1.5f;
+    private final static int MIN_PRICE_PER_HOUR = 8;
+    private final static int MAX_HOURS_PER_WEEK = 60;
+    private final static int LIMIT_HOURS_TO_KOEFF = 40;
+    private final static BigDecimal AFTER_LIMIT_KOEFFICIENT = new BigDecimal(1.5);
+    private final static int FRACTION_DIGITS = 2;
 
 
-    public static float calculatePay(float pricePerHour, float workingHours) throws HoursOverLimitException, PriceOverLimitException {
+    public static BigDecimal calculatePay(BigDecimal pricePerHour, BigDecimal workingHours) throws HoursOverLimitException, PriceOverLimitException {
         validateData(pricePerHour, workingHours);
-        float paydHours = PayCalculator.calculatePaydHours(workingHours);
-        return paydHours * pricePerHour;
+        BigDecimal paydHours = PayCalculator.calculatePaydHours(workingHours);
+        return pricePerHour.multiply(paydHours).setScale(FRACTION_DIGITS, BigDecimal.ROUND_HALF_UP);
     }
 
-    private static float calculatePaydHours(float workingHours) {
-        if (workingHours <= LIMIT_HOURS_TO_KOEFF) {
+    private static BigDecimal calculatePaydHours(BigDecimal workingHours) {
+        if (workingHours.compareTo(new BigDecimal(LIMIT_HOURS_TO_KOEFF)) <= 0) {
             return workingHours;
         } else {
-            float overShapeHours = workingHours - LIMIT_HOURS_TO_KOEFF;
-            return LIMIT_HOURS_TO_KOEFF + overShapeHours * AFTER_LIMIT_KOEFFICIENT;
+            BigDecimal overShapeHours = workingHours.add(new BigDecimal(LIMIT_HOURS_TO_KOEFF * -1));
+            return overShapeHours.multiply(AFTER_LIMIT_KOEFFICIENT).add(new BigDecimal(LIMIT_HOURS_TO_KOEFF));
         }
     }
 
-    private static void validateData(float pricePerHour, float workingHours) throws HoursOverLimitException, PriceOverLimitException {
-        if (pricePerHour < MIN_PRICE_PER_HOUR) {
-            throw new PriceOverLimitException(String.format("Price %f lower then minimum %f", pricePerHour, MIN_PRICE_PER_HOUR));
+    private static void validateData(BigDecimal pricePerHour, BigDecimal workingHours) throws HoursOverLimitException, PriceOverLimitException {
+        if (pricePerHour.compareTo(new BigDecimal(MIN_PRICE_PER_HOUR)) == -1) {
+            throw new PriceOverLimitException(String.format("Price %f lower then minimum %d", pricePerHour, MIN_PRICE_PER_HOUR));
         }
-        if (workingHours > MAX_HOURS_PER_WEEK) {
-            throw new HoursOverLimitException(String.format("Working hours %f higher then maximum %f", workingHours, MAX_HOURS_PER_WEEK));
+        if (workingHours.compareTo(new BigDecimal(MAX_HOURS_PER_WEEK)) == 1) {
+            throw new HoursOverLimitException(String.format("Working hours %f higher then maximum %d", workingHours, MAX_HOURS_PER_WEEK));
         }
     }
 
